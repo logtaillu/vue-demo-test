dbc4ef730d8db0377164ef56b7b0e3c2e3c4db7b
logtaillu <tailuuu@qq.com>
2025-09-09 18:27:17 +0800

    feat: init

---
+++ src/components/liquid-glass/liquid-glass.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="click">{{ msg }}</button>
+</template>
+
+<script>
+export default {
+  name: 'LiquidGlass',
+  props: {
+    msg: {
+      type: String,
+      default: 'Liquid Glass'
+    }
+  },
+  computed: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    click() {
+      console.log('clicked')
+    }
+  },
+  mounted() {
+    console.log('mounted')
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  button
+    padding: 10px 20px
+    border-radius: 5px
+    border: none
+    background-color: #42b983
+    color: #fff
+    font-size: 16px
+    cursor: pointer
+</style>
